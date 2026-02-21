aef73c204b27252b6947e62b2f62edb1faa70f02
Onniikun <dominick2004natoza@gmail.com>
2026-02-21 03:57:41 -0600

    Iterated data from external database.

--- src/component/common/course_registration/Calender/Calender.jsx
+++ src/component/common/course_registration/Calender/Calender.jsx
@@ -8,28 +8,52 @@ import './Calender.css'
 
 export function Calender() {
     const eventsService = useState(() => createEventsServicePlugin())[0]
-    const calender = useCalendarApp({
-    views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
-    events: [
-        {
-        id: 1,
-        title: 'Class 1',
-        start: Temporal.PlainDate.from({
-            year: 2026,
-            month: 2,
-            day: 16,
-            hour: 9,
-            minute: 0
-        }),
-        end: Temporal.PlainDate.from({
-            year: 2026,
-            month: 2,
-            day: 16,
-            hour: 10,
-            minute: 0
-        }),
+
+    //mockdata 
+    const Courses = [
+        {   
+            id: '1', 
+            title: 'Math 101', 
+            startDate: { year: 2026, month: 2, day: 22 }, 
+            startTime: 500, 
+            endDate: { year: 2026, month: 2, day: 22 }, 
+            endTime: 700 
         },
-    ],
+    ]
+
+    // Hour/Minute Converter
+    const getTime = (minutes) => ({
+        hour: Math.floor(minutes / 60),
+        minute: minutes % 60
+    })
+    const events = Courses.map(course => {
+        const { hour: startHour, minute: startMinute } = getTime(course.startTime)
+        const { hour: endHour, minute: endMinute } = getTime(course.endTime)
+
+        return {
+                id: course.id,
+                title: course.title,
+                start: Temporal.ZonedDateTime.from({
+                year: course.startDate.year,
+                month: course.startDate.month,
+                day: course.startDate.day,
+                hour: startHour,
+                minute: startMinute,
+                timeZone: 'America/Winnipeg'
+            }),
+                end: Temporal.ZonedDateTime.from({
+                year: course.endDate.year,
+                month: course.endDate.month,
+                day: course.endDate.day,
+                hour: endHour,
+                minute: endMinute,
+                timeZone: 'America/Winnipeg'
+            })
+        }
+        })
+
+    const calender = useCalendarApp({views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
+    events: events,
     plugins: [eventsService]
 });
 useEffect(() => {
